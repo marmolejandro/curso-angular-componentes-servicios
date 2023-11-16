f6b7ec73c6e4d026af0ff576180189ae3a44f0b0
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-16 15:06:23 -0500

    - Se crea componente "product" para implementarlo desde el componente app principal. - Se define el modelo Product para representar y tipar los datos de un producto

--- my-store/src/app/components/img/img.component.ts
+++ my-store/src/app/components/img/img.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-img',
@@ -10,4 +11,17 @@ import { CommonModule } from '@angular/common';
 })
 export class ImgComponent {
 
+  @Input() img: string = 'valor init';
+  @Output() loaded = new EventEmitter<string>();
+
+  imageDefault = 'https://www.m2crowd.com/core/i/placeholder.png';
+
+  imgError(){
+    this.img = this.imageDefault
+  }
+
+  @Output() imgLoaded(){
+    console.log('Log hijo')
+    this.loaded.emit(this.img);
+  }
 }
