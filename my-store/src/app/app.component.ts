592e1573bb242b163f6c0c55b7faa35d2641c413
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-16 08:31:31 -0500

    Se inicializa nuevo componente "img"

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ImgComponent } from './components/img/img.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ImgComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ my-store/src/app/components/img/img.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-img',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './img.component.html',
+  styleUrl: './img.component.scss'
+})
+export class ImgComponent {
+
+}
