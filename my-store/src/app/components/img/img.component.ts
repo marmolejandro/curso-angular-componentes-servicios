761454cb0d7d93adcab17c9744fad23ac2930901
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-17 08:15:44 -0500

    Se implementan diferentes ciclos al componente de imagen para ponerlos a prueba

--- my-store/src/app/components/img/img.component.ts
+++ my-store/src/app/components/img/img.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import { EventEmitter } from 'stream';
 
@@ -9,7 +18,39 @@ import { CommonModule } from '@angular/common';
   templateUrl: './img.component.html',
   styleUrl: './img.component.scss'
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
+
+  // ---------------------------------------------------------------------
+  // Ciclos
+  // ---------------------------------------------------------------------
+  constructor(){
+    // before render
+    // NO async -- once time
+    console.log('Constructor: ', 'imgValue => ', this.img);
+  }
+  ngOnChanges(): void {
+    // before & during render
+    // inputs changes -- many times - each change
+    console.log('ngOnChanges: ', 'imgValue => ', this.img);
+  }
+  ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit: ', 'imgValue => ', this.img);
+  }
+  // Grupo ngDoCheck
+  ngAfterViewInit(): void {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+  ngOnDestroy(): void {
+    // delete component
+    console.log('ngOnDestroy');
+  }
+
+  // ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------
 
   @Input() img: string = 'valor init';
   @Output() loaded = new EventEmitter<string>();
