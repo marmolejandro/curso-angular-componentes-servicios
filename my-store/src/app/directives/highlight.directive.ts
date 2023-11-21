d60c7d52d5b26ae7fc78b99a42e9fa8108c69e87
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-21 11:57:36 -0500

    Se implementan escuchas para la directiva creada appHighlight

--- my-store/src/app/directives/highlight.directive.ts
+++ my-store/src/app/directives/highlight.directive.ts
@@ -1,14 +1,23 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
+  @HostListener('mouseenter') onMouseEnter(){
+
+    this.element.nativeElement.style.backgroundColor = 'darkgray';
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+
+    this.element.nativeElement.style.backgroundColor = '';
+  }
+
   constructor(
     private element: ElementRef
   ) {
-    this.element.nativeElement.style.backgroundColor = 'darkgray';
+    // this.element.nativeElement.style.backgroundColor = 'darkgray';
   }
 
 }
