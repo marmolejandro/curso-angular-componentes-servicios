c271a5fd8fdb3becd6fbb9b9c1f8f8068b5b78ec
CEIBA\alejandro.marmolejo <alejando.marmolejo@ceiba.com.co>
2023-11-21 11:53:54 -0500

    - Se crea un pipe personalizado - Se implementa el uso de directivas para modificar el DOM

--- my-store/src/app/app.module.ts
+++ my-store/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TimeAgoPipe } from './pipes/time-ago.pipe';
     ProductsComponent,
     NavComponent,
     ReversePipe,
-    TimeAgoPipe
+    TimeAgoPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
